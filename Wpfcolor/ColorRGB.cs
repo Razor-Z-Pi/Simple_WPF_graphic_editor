241aef4e176fac0ada2a1049ae58a8fe458f8877
Razor_Z_Pi <74016324+Razor-Z-Pi@users.noreply.github.com>
2021-05-13 16:22:52 +0700

    Add files via upload

---
+++ Wpfcolor/ColorRGB.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Wpfcolor
+{
+
+    public class ColorRGB
+    {
+        public byte red { get; set; }
+        public byte green { get; set; }
+        public byte blue { get; set; }
+    }
+}
